14de7693a8150117394e0498724c56eb21afcdcb
Souren KS <sourenks685@gmail.com>
2026-04-16 09:20:02 +0530

    Update syscalls.h

--- include/vfs/syscalls.h
+++ include/vfs/syscalls.h
@@ -9,5 +9,7 @@ int sys_open(const char *path, uint32_t mode);
 ssize_t sys_read(int fd, void *buf, size_t count);
 ssize_t sys_write(int fd, const void *buf, size_t count);
 int sys_close(int fd);
+int sys_mkdir(const char *parent_path, const char *dir_name, uint16_t mode);
+int sys_rmdir(const char *parent_path, const char *dir_name);
 
 #endif
