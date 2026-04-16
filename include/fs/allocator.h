81ee13194fe595f7814ab0f459427531aa79457e
Souren KS <sourenks685@gmail.com>
2026-04-16 09:21:27 +0530

    Update allocator.h

--- include/fs/allocator.h
+++ include/fs/allocator.h
@@ -6,4 +6,7 @@
 int fs_alloc_block(uint32_t bg_num, uint32_t *allocated_block);
 int fs_alloc_inode(uint32_t bg_num, uint32_t *allocated_inode);
 
+int fs_free_block(uint32_t bg_num, uint32_t block_num);
+int fs_free_inode(uint32_t bg_num, uint32_t inode_num);
+
 #endif
